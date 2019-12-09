0914a6e0b749d35c46905f27a853519f64b291a7
AVLlarsson <annlar2@hotmail.com>
2019-12-09 18:37:22 +0100

    styles css

---
+++ script.js
@@ -0,0 +1,9 @@
+/*function timelineCheck() {
+    var element = document.getElementById("tempLine");
+    var screenWidth = $(document).width();
+
+    if (screenWidth < 600) {
+        element.parentNode.removeChild(element);
+    }
+}
+*/
