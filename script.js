c459cd429231496ddfb34a3ba93ddc0349e23ae1
AVLlarsson <annlar2@hotmail.com>
2019-12-17 14:11:00 +0100

    style progress, mobile done and working on tablet

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
+window.addEventListener('load', typeWriter());
+
 var imgAnimation = gsap.timeline();
 
-gsap.from("body", { duration: 1, opacity: 0 })
+gsap.from("body", { duration: 1, opacity: 0 });
 
 imgAnimation /* Picture animation */
-    .from(".portrait", { duration: 2, scale: 0.80 })
+    .from(".portrait", { duration: 2, scale: 0.80 });
+
+
+
+function typeWriter() { /* Typewriter style on H1 (name) */
+    let i = 0;
+    const name = "ANNIE LARSSON";
+    const velocity = 150;
+    const element = document.getElementById("mName");
+    const interval = setInterval(addSymbol, velocity);
+
+    function addSymbol() {
+        if (i < name.length) {
+            element.innerHTML += name.charAt(i);
+            i++;
+        } else {
+            clearInterval(interval);
+        }
+    }
+}
